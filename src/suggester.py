24964f44f2616b9029ffa0fa36d3cb172f8ca9ec
Sully <48493571+dizzyfrogs@users.noreply.github.com>
2025-07-23 23:18:54 -0500

    add SpellingSuggester class using BK-tree and edit distance

---
+++ src/suggester.py
@@ -0,0 +1,18 @@
+from src.bktree import BKTree
+from src.edit_distance import edit_distance
+
+class SpellingSuggester:
+    def __init__(self, word_list):
+        """
+        Build BK-tree using the given word list.
+        """
+        self.tree = BKTree(edit_distance)
+        for word in word_list:
+            self.tree.insert(word.lower())
+
+    def suggest(self, word, tolerance=2):
+        """
+        Suggest words from the dictionary within the given edit distance tolerance.
+        Returns a list of (word, distance) tuples, sorted by distance.
+        """
+        return self.tree.search(word.lower(), tolerance)
